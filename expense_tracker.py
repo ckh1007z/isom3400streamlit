50ea89413b9eb5e68dfa208fe4255954f75205ff
ckh1007z <khcheungba@connect.ust.hk>
2025-12-05 12:26:52 +0800

    Create expense_tracker.py

---
+++ expense_tracker.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyploy as plt
+
+st.title("Personal Expense Tracker")
+if 'expenses' not in st.session_state:
+  st.session_state.expenses = pd.dataframe(columns=['Date','Category','Amount','Description'])
+
+with st.form("expense_form"):
+  st.subheader("Add New Expense")
+  date = st.date_input("Date")
+  category = st.selectbox("Category",["Bills","Transport","Food"])
+  amount = st.number_input("Amount", min_value = 0.00 , step = 0.01)
+  description = st.text_input("Description")
+
+submitted = st.form_submit_button("Add Expense")
+if submitted is True:
+  new_expense = pd.DataFrame({
+    'Date': [date],
+    'Category': [category],
+    'Amount': [amount],
+    'Description': [description]
+  })
+  st.session_state.expenses = pd.concat([st.session_state.expenses, new_expense], ignore_index = True)
+  st.success("Expenses added!")
+
+if not st.session_state.expenses.empty:
+  st.subheader("Your Expenses")
+  st.dataframe(st.session_state.expenses)
+
+  st.subheader("Summary")
+  total_spent = st.session_state.expenses['Amount'].sum()
+  st.write(f"Total Spent: ${total_spent:.2f}.")
+
+  category_totals = st.session_state.expenses.groupby('Category')["Amount"].sum()
+
+  fig, ax = plt.subbplots(figsize=(10,6))
+  ax.pie(category_totals.value, labels=category_totals.index, autopct='%1.1f%%')
+  ax.set_title("Expenses by Category")
+  st.pyplot(fig)
+else:
+  st.info("No expense added! Please add some")
+  
+
+
+  
+
+  
