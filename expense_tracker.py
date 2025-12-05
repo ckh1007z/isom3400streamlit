8dec830744d220885ca3e6788d0f76802ae33433
ckh1007z <khcheungba@connect.ust.hk>
2025-12-05 12:32:31 +0800

    Update expense_tracker.py

--- expense_tracker.py
+++ expense_tracker.py
@@ -7,22 +7,22 @@ if 'expenses' not in st.session_state:
   st.session_state.expenses = pd.dataframe(columns=['Date','Category','Amount','Description'])
 
 with st.form("expense_form"):
-  st.subheader("Add New Expense")
-  date = st.date_input("Date")
-  category = st.selectbox("Category",["Bills","Transport","Food"])
-  amount = st.number_input("Amount", min_value = 0.00 , step = 0.01)
-  description = st.text_input("Description")
-
-  submitted = st.form_submit_button("Add Expense")
-  if submitted is True:
-    new_expense = pd.DataFrame({
-      'Date': [date],
-      'Category': [category],
-      'Amount': [amount],
-      'Description': [description]
-    })
-    st.session_state.expenses = pd.concat([st.session_state.expenses, new_expense], ignore_index = True)
-    st.success("Expenses added!")
+    st.subheader("Add New Expense")
+    date = st.date_input("Date")
+    category = st.selectbox("Category",["Bills","Transport","Food"])
+    amount = st.number_input("Amount", min_value = 0.00 , step = 0.01)
+    description = st.text_input("Description")
+  
+    submitted = st.form_submit_button("Add Expense")
+    if submitted is True:
+      new_expense = pd.DataFrame({
+        'Date': [date],
+        'Category': [category],
+        'Amount': [amount],
+        'Description': [description]
+      })
+      st.session_state.expenses = pd.concat([st.session_state.expenses, new_expense], ignore_index = True)
+      st.success("Expenses added!")
 
 if not st.session_state.expenses.empty:
   st.subheader("Your Expenses")
